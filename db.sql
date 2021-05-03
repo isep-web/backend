2e82edfb6227ed4ef6de3029e4557e3bec046e2f
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-05-04 01:48:18 +0800

    update db.sql

--- db.sql
+++ db.sql
@@ -30,11 +30,11 @@ CREATE TABLE `application` (
   `to` bigint NOT NULL,
   `id_house` bigint NOT NULL,
   `is_accepted` tinyint NOT NULL DEFAULT '0',
-  `start_date` timestamp NULL DEFAULT NULL,
-  `end_date` timestamp NULL DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
   `guests` tinyint DEFAULT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idapplication_UNIQUE` (`id`),
   KEY `app_user_from_idx` (`from`),
@@ -63,16 +63,16 @@ DROP TABLE IF EXISTS `auth`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `auth` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL,
   `role` set('admin','user') NOT NULL DEFAULT 'user',
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -81,7 +81,7 @@ CREATE TABLE `auth` (
 
 LOCK TABLES `auth` WRITE;
 /*!40000 ALTER TABLE `auth` DISABLE KEYS */;
-INSERT INTO `auth` VALUES (1,'aaa','pass','user','2021-05-03 08:34:58',NULL);
+INSERT INTO `auth` VALUES (1,'aaa','pass','user','2021-05-03 16:34:58',NULL),(3,'aaaaa','pass1','user','2021-05-03 18:17:55',NULL),(4,'bbb','pass2','user','2021-05-03 18:17:55',NULL),(6,'summerday','123456','admin','2021-05-03 18:23:00',NULL),(7,'asdasd','123456','user','2021-05-03 18:25:04','2021-05-03 18:30:48'),(11,'test1','123123','user','2021-05-04 01:13:19','2021-05-04 01:13:19');
 /*!40000 ALTER TABLE `auth` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -103,8 +103,8 @@ CREATE TABLE `house` (
   `rules` json DEFAULT NULL,
   `is_verified` tinyint NOT NULL DEFAULT '0',
   `guests` tinyint NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `house_user_idx` (`id_user`),
@@ -135,8 +135,8 @@ CREATE TABLE `message` (
   `type` varchar(45) NOT NULL,
   `is_readed` tinyint NOT NULL,
   `content` json NOT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `idmessage_UNIQUE` (`id`),
   KEY `msg_user_fk_from_idx` (`from`),
@@ -172,11 +172,11 @@ CREATE TABLE `user` (
   `language` varchar(100) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `location` geometry DEFAULT NULL,
-  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `last_updated_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_date` datetime DEFAULT NULL,
+  `last_updated_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  CONSTRAINT `user_auth` FOREIGN KEY (`id`) REFERENCES `auth` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+  CONSTRAINT `user_auth` FOREIGN KEY (`id`) REFERENCES `auth` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -186,7 +186,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'user1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2021-04-27 16:00:00',NULL);
+INSERT INTO `user` VALUES (1,'user1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2021-04-28 00:00:00',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -199,4 +199,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-03 16:43:04
+-- Dump completed on 2021-05-04  1:47:46
