bde866e0c5484b2c869492be1c0c7fede66209b8
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-05-05 03:38:45 +0800

    add javadoc

--- src/main/java/com/isep/project/ProjectApplication.java
+++ src/main/java/com/isep/project/ProjectApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * @author : Xuan MIAO
+ * @version : 1.0.0
+ * @date : 2021/5/5
+ */
 @EnableJpaAuditing
 @SpringBootApplication
 public class ProjectApplication
