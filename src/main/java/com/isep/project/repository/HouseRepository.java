0dc142fdb097be57bea44c130da7e1cc05e7e934
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-05-31 23:52:59 +0800

    高级搜索

--- src/main/java/com/isep/project/repository/HouseRepository.java
+++ src/main/java/com/isep/project/repository/HouseRepository.java
@@ -1,11 +1,13 @@
 package com.isep.project.repository;
 
 import com.isep.project.entity.House;
-import java.util.Collection;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author : Xuan MIAO
@@ -15,7 +17,21 @@ import org.springframework.data.repository.query.Param;
 public interface HouseRepository extends JpaRepository<House, Long>, JpaSpecificationExecutor<House>
 {
 
-    List<House> findByServicesIdIn(@Param("ids") Collection<Long> id);
+    List<House> findByUserId(@Param("id") Long id);
 
-    List<House> findByServicesId(@Param("id") Long id);
+    @RestResource(exported = false)
+    @Query(value = "SELECT DISTINCT(h.id) FROM House h"
+            + " WHERE((:areaMin is null OR :areaMax is null OR :areaMin='' OR :areaMax='') "
+            + "OR h.area BETWEEN :areaMin AND :areaMax ) "
+            + "AND (:title is null OR :title='' OR h.title like CONCAT('%',:title,'%'))"
+            + "AND (:guestNumber is null OR :guestNumber='' OR h.guestNumber >=:guestNumber)"
+            + "AND (h.id in (SELECT h.id FROM House h left JOIN h.amenities ha where ha.id in "
+            + "(:amenities) group by h.id having count(h)= :amenitiesNum))"
+    )
+    List<Long> advSearch(@Param("areaMin") String areaMin,
+            @Param("areaMax") String areaMax, @Param("title") String title,
+            @Param("guestNumber") String guestNumber, @Param("amenities") List<Long> amenities,
+            @Param("amenitiesNum") Long amenitiesNum,
+            Pageable page);
+
 }
---
+++ src/main/java/com/isep/project/service/HouseService.java
@@ -0,0 +1,38 @@
+package com.isep.project.service;
+
+import com.isep.project.entity.House;
+import com.isep.project.repository.HouseRepository;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : Xuan MIAO
+ * @version : 1.0.0
+ * @date : 2021/5/31
+ */
+@Service
+public class HouseService
+{
+
+    @Resource
+    private HouseRepository houseRepository;
+
+    public List<Long> advancedSearch(String areaMin, String areaMax, String title,
+            String guestNumber,
+            String amenities, Pageable page)
+    {
+        String[] amenitiesList = amenities.split(",");
+        List<Long> amenitiesLong = new ArrayList<>();
+        for (String service : amenitiesList)
+        {
+            amenitiesLong.add(Long.parseLong(service));
+        }
+        return houseRepository
+                .advSearch(areaMin, areaMax, title, guestNumber, amenitiesLong,
+                        (long) amenitiesLong.size(),
+                        page);
+    }
+}
