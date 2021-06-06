15d30609375d5fb46242b6b5cd743e9f45ca9d53
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-06-06 22:58:11 +0800

    fix CORS

--- src/main/java/com/isep/project/repository/PictureStoreRepository.java
+++ src/main/java/com/isep/project/repository/PictureStoreRepository.java
@@ -3,10 +3,12 @@ package com.isep.project.repository;
 import com.isep.project.entity.Picture;
 import org.springframework.content.commons.repository.ContentStore;
 import org.springframework.content.rest.StoreRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author Xuan MIAO
  */
+@CrossOrigin
 @StoreRestResource(path = "pictures", linkRel = "pictures")
 public interface PictureStoreRepository extends ContentStore<Picture, String>
 {
