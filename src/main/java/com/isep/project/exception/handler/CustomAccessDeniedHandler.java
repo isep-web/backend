edc92d78de1130ec0d9bca4d6361cb4ecda17891
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-06-10 22:37:35 +0800

    bug fix

--- src/main/java/com/isep/project/exception/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/isep/project/exception/handler/CustomAccessDeniedHandler.java
@@ -1,6 +1,27 @@
-package com.isep.project.exception.handler;/**
+package com.isep.project.exception.handler;
+
+import com.isep.project.common.Status;
+import com.isep.project.service.ResponseService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+/**
  * @author : Xuan MIAO
- * @date : 2021/6/10
  * @version : 1.0.0
- */public class CustomAccessDeniedHandler {
+ * @date : 2021/6/10
+ */
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler
+{
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+            AccessDeniedException accessDeniedException)
+    {
+        ResponseService
+                .renderJson(response, Status.ACCESS_DENIED, null);
+    }
+
 }
